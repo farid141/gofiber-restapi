8d715bd918c610a1745516dc467fbe533f038c2b
farid <faridnubaili@gmail.com>
2025-08-13 18:49:27 +0700

    add jwt cookies auth

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"time"
+
+	jwtware "github.com/gofiber/contrib/jwt"
+	"github.com/golang-jwt/jwt/v5"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
@@ -19,10 +24,60 @@ func main() {
 	file, _ := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	log.SetOutput(file)
 
+	// Login route
+	app.Post("/login", login)
+
+	// Unauthenticated route
 	app.Get("/", func(c *fiber.Ctx) error {
 		log.Info("hay")
 		return c.SendString("Hello, World!")
 	})
 
+	// JWT Middleware
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey:  jwtware.SigningKey{Key: []byte("secret")},
+		TokenLookup: "cookie:token", // 👈 look in cookie named "token"
+	}))
+
+	// Restricted Routes
+	app.Get("/restricted", restricted)
+
 	app.Listen(":3000")
+}
+
+func login(c *fiber.Ctx) error {
+	fmt.Println("login")
+	user := c.FormValue("user")
+	pass := c.FormValue("pass")
+
+	// Throws Unauthorized error
+	if user != "john" || pass != "doe" {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	// Create the Claims
+	claims := jwt.MapClaims{
+		"name":  "John Doe",
+		"admin": true,
+		"exp":   time.Now().Add(time.Hour * 72).Unix(),
+	}
+
+	// Create token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	c.Cookie(&fiber.Cookie{Name: "token", Value: t})
+	return c.JSON(fiber.Map{"token": t})
+}
+
+func restricted(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	return c.SendString("Welcome " + name)
 }
